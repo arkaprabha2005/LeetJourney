c95fac272de21686f9f9be8bab41dd766c0f9525
Arkaprabha Pal <arka200515@gmail.com>
2026-06-23 20:57:55 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.06%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,13 +1,28 @@
-class Solution {
-    HashMap<Integer,Integer> map=new HashMap<>();
+// class Solution {//Memoization
+//     HashMap<Integer,Integer> map=new HashMap<>();
+//     public int tribonacci(int n) {
+//         if(map.containsKey(n)) return map.get(n);
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         if(n==2) return 1;
+//         int ans=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+//         map.put(n,ans);
+//         return ans;
+//     }
+    
+// }
+
+class Solution {//Tabulation
     public int tribonacci(int n) {
-        if(map.containsKey(n)) return map.get(n);
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 1;
-        int ans=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-        map.put(n,ans);
-        return ans;
+         if(n==0) return 0;
+        if(n<=2) return 1;
+        int[] arr=new int[n+1];
+        arr[0]=0;
+        arr[1]=arr[2]=1;
+        for(int i=3;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        return arr[n];
     }
     
 }
