4a9daff645167b60789a02a2ed68642ba2e5917f
Arkaprabha Pal <arka200515@gmail.com>
2026-06-23 21:49:42 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (31.32%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {//Boyre Moore Voting algorithm
+        int candidate=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(count==0){
+                candidate=nums[i];
+            }
+            if(candidate==nums[i]){
+                count++;
+            }
+            else {
+                count--;
+                
+            }
+        }
+        return candidate;
+    }
+}
