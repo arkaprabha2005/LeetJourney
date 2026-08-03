971015037761442bdcdf171c6ddd7b31e5f293a7
Arkaprabha Pal <arka200515@gmail.com>
2026-08-03 21:09:43 +0530

    Time: 1 ms (8.51%), Space: 43.6 MB (34.23%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,23 +13,54 @@
  *     }
  * }
  */
+
+ //-----------------------Recursive
+
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         return mirror(root.left,root.right);
+//     }
+
+//     private boolean mirror(TreeNode l,TreeNode r){
+//         if(l==null && r==null) return true;
+
+//         if(l==null || r==null) return false;
+        
+//         if(l.val!=r.val) return false;
+        
+//         boolean left=mirror(l.left,r.right);
+//         boolean right=mirror(l.right,r.left);
+
+//         return left && right;
+        
+
+//     }
+// }
+
+//-----------------Iterative
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return mirror(root.left,root.right);
-    }
+        if(root==null) return true;
+        Queue<TreeNode> q1=new LinkedList<>();
+        if(root.left==null && root.right==null) return true;
+        if(root.left==null || root.right==null) return false;
+        q1.offer(root.left);
+        q1.offer(root.right);
+        while(!q1.isEmpty()){
+            TreeNode l=q1.poll();
+            TreeNode r=q1.poll();
+            if(l==null && r==null) continue;
+            if(r==null ||l==null) return false;
+            if(l.val!=r.val) return false;
+            q1.offer(l.left);
+            q1.offer(r.right);
 
-    private boolean mirror(TreeNode l,TreeNode r){
-        if(l==null && r==null) return true;
+            q1.offer(l.right);
+            q1.offer(r.left);
+        }
 
-        if(l==null || r==null) return false;
-        
-        if(l.val!=r.val) return false;
-        
-        boolean left=mirror(l.left,r.right);
-        boolean right=mirror(l.right,r.left);
-
-        return left && right;
-        
+        return true;
 
     }
 }
