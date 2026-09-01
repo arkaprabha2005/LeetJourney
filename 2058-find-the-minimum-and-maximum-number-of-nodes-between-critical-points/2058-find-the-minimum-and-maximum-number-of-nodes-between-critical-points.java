0025d48cc3251f90c50862ca299ad4159f07d711
Arkaprabha Pal <arka200515@gmail.com>
2026-09-01 11:47:02 +0530

    Time: 4 ms (99.87%), Space: 106 MB (70.17%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -13,35 +13,33 @@ class Solution {
         //int[] arr={-1,-1};
         //ArrayList<Integer> l1=new ArrayList<>;
         ListNode n1;
-        ListNode node=head;
-        int loc=1;
-        int prev=0;
-        int max=0;
-        int flag=-1;
-        int min=Integer.MAX_VALUE;
-        while(node.next.next!=null){
+        ListNode node = head;
+        int loc = 1;
+        int prev = 0;
+        int max = 0;
+        int flag = -1;
+        int min = Integer.MAX_VALUE;
+        while (node.next.next != null) {
             //n1=node;
             //node=node.next;
             loc++;
-            
-            if ((node.val > node.next.val && node.next.next.val > node.next.val) ||
-                (node.val < node.next.val && node.next.next.val < node.next.val)) {
-                if(flag==-1){
-                    flag=loc;
+
+            if ((node.val > node.next.val && node.next.next.val > node.next.val) || (node.val < node.next.val && node.next.next.val < node.next.val)) {
+                if (flag == -1) {
+                    flag = loc;
+                } else {
+                    min = Math.min(min, loc - prev);
                 }
-                else{
-                min=Math.min(min,loc-prev);
-                }
-                prev=loc;
+                prev = loc;
             }
-            node=node.next;
+            node = node.next;
         }
-        if(flag==-1){
-            return new int[]{-1,-1};
+        if (flag == -1) {
+            return new int[] { -1, -1 };
         }
-        if(flag==prev){
-            return new int[]{-1,-1};
+        if (flag == prev) {
+            return new int[] { -1, -1 };
         }
-        return new int[]{min,prev-flag};
+        return new int[] { min, prev - flag };
     }
 }
